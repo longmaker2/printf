86e102e5e072558a7327c0240649e2a20cba1034
Gabriel Muthenya <gmthex@gmail.com>
2023-03-28 09:03:16 -0700

    Update main.h file

--- main.h
+++ main.h
@@ -1,7 +1,41 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
 /* _printf */
 int _printf(const char *format, ...);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
 
 #endif
